3809db500dd52d1844b3c728953f4549e84e9caf
Chris Tague <cstague6401@gmail.com>
2026-03-01 01:44:27 -0600

    read some dumb data

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <optional>
 #include <string>
+#include <vector>
+
+std::optional<int> extract_num(std::string str) {
+    std::optional<int> num = std::nullopt;
+
+    try {
+        num = std::stoi(str);
+    } catch (const std::invalid_argument& e) {
+        std::cerr << "Invalid input: " << str << std::endl;
+    } catch (const std::out_of_range& e) {
+        std::cerr << "Number too large" << std::endl;
+    }
+
+    return num;
+}
 
 int main() {
-    // std::ifstream inputFile = std::ifstream("data.txt");
-    // std::ifstream inputFile{"data.txt"};
     std::ifstream inputFile("data.txt");
 
     if (!inputFile.is_open()) {
@@ -12,10 +26,38 @@ int main() {
         return 1;
     }
 
+    std::vector<std::vector<std::optional<int>>> sudoku;
+
     std::string line;
 
     while (std::getline(inputFile, line)) {
-        std::cout << line << std::endl;
+        std::string num_buffer = "";
+        std::vector<std::optional<int>> sudoku_line;
+
+        for (unsigned long i = 0; i < line.length(); i++) {
+            if (line[i] == ' ') {
+                auto num = extract_num(num_buffer);
+                num_buffer = "";
+                sudoku_line.push_back(num);
+
+                continue;
+            }
+
+            num_buffer.push_back(line[i]);
+        }
+
+        auto num = extract_num(num_buffer);
+        sudoku_line.push_back(num);
+        sudoku.push_back(sudoku_line);
+        sudoku_line.clear();
+    }
+
+    for (const auto& sudoku_line : sudoku) {
+        for (const auto& num : sudoku_line) {
+            std::cout << num.value_or(0);
+        }
+
+        std::cout << std::endl;
     }
 
     return 0;
