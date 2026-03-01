b5c8fa8323ba5b7fd395ff8abdab2713c6909cad
Chris Tague <cstague6401@gmail.com>
2026-03-01 00:14:19 -0600

    reading files

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::ifstream inputFile("data.txt");
+
+    if (!inputFile.is_open()) {
+        std::cerr << "Error: Could not open file!" << std::endl;
+        return 1;
+    }
+
+    std::string line;
+
+    while (std::getline(inputFile, line)) {
+        std::cout << line << std::endl;
+    }
+
     return 0;
 }
